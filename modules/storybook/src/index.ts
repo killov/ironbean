7bc30b6713815da29360afa5a20af6186ac04fb7
Zdeněk Mazurák <z.mazurak@seznam.cz>
2020-05-03 18:01:08 +0200

    storybook oninit

--- modules/storybook/src/index.ts
+++ modules/storybook/src/index.ts
@@ -1,1 +1,17 @@
+import {getBaseTestingContext, TestingContext} from "fire-dic";
 
+type TOnInit = (context: TestingContext) => void;
+
+export let onI: TOnInit | undefined;
+
+export function onInitStory(onInit: TOnInit) {
+    onI = onInit;
+    start();
+}
+
+export function start() {
+    if (onI) {
+        const context = getBaseTestingContext();
+        onI(context);
+    }
+}
--- modules/storybook/src/register.ts
+++ modules/storybook/src/register.ts
@@ -1,8 +1,10 @@
 import addons from "@storybook/addons";
 import {destroyContext} from "fire-dic";
+import {start} from "./index";
 
 addons.register("fire-dic", (api) => {
     api.onStory(() => {
         destroyContext();
+        start();
     });
 });
