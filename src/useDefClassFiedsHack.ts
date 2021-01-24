9cfbd9c9874cb495094a8ffed7cdfc6ebbba8b8f
Zdeněk Mazurák <z.mazurak@seznam.cz>
2021-01-24 21:50:43 +0100

    podpora useDefineForClassFields fix dedicnosti

--- src/useDefClassFiedsHack.ts
+++ src/useDefClassFiedsHack.ts
@@ -1,7 +1,7 @@
 const propOverridesSymbol = Symbol()
 const origDefineProperty = Object.defineProperty;
 Object.defineProperty = function(o: any, p: string | number | symbol, attributes: PropertyDescriptor & ThisType<any>): any {
-    const overriden = o[propOverridesSymbol]?.has(p); // will get it from prototype if available
+    const overriden =isOverriten(o, p); // will get it from prototype if available
     if (!overriden) {
         return origDefineProperty(o, p, attributes);
     } else {
@@ -9,6 +9,14 @@ Object.defineProperty = function(o: any, p: string | number | symbol, attributes
         o[p] = attributes.value;
         return o;
     }
+}
+
+function isOverriten(o: any, p: string | number | symbol): boolean {
+    const set = o[propOverridesSymbol];
+    if (set) {
+        return set.has(p) || isOverriten(Object.getPrototypeOf(o), p)
+    }
+    return false;
 }
 
 export function markAsOverridenDefineProperty(o: any, p: string | number | symbol) {
