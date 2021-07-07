6f2c25246f96eaf08d5d36fbf041445c923c8a32
Zdeněk Mazurák <z.mazurak@seznam.cz>
2021-07-07 16:58:50 +0200

    refoldering

--- modules/core/karma.conf.js
+++ modules/core/karma.conf.js
@@ -2,8 +2,8 @@ module.exports = function (config) {
     config.set({
         frameworks: ["jasmine", "karma-typescript"],
         files: [
-            "./src/*.ts", // *.tsx for React Jsx
-            "./test/*.ts" // *.tsx for React Jsx
+            "src/*.ts", // *.tsx for React Jsx
+            "test/*.ts" // *.tsx for React Jsx
         ],
         preprocessors: {
             "*/*.ts": ["karma-typescript"]
