913b9ac7283fdb53672fcd68e2a54942f51e6537
Zdeněk Mazurák <z.mazurak@seznam.cz>
2021-10-05 20:49:23 +0200

    auto factory

---
+++ packages/ironbean/src/autofactory.ts
@@ -0,0 +1,40 @@
+import {
+    ApplicationContext,
+    autowired,
+    component,
+    Component,
+    ComponentType,
+    Dependency,
+    getDefaultScope,
+    scope,
+    Scope,
+    TClass
+} from "./internals";
+
+interface AutoFactory<T> {
+    create(): T;
+}
+
+interface ISettings {
+    scope?: Scope;
+}
+
+export function AutoFactory<T>(dependency: Dependency<T>, settings?: ISettings): TClass<AutoFactory<T>> {
+    const cmp = Component.create(dependency);
+
+    if (cmp.isConstructable()) {
+        throw new Error(cmp.name + " for auto factory must be @component");
+    }
+    if (cmp.getComponent().getType() !== ComponentType.Prototype) {
+        throw new Error(cmp.name + " for auto factory must be ComponentType.Prototype");
+    }
+
+    return @component @scope(settings?.scope ?? getDefaultScope()) class implements AutoFactory<T> {
+        @autowired
+        private context: ApplicationContext;
+
+        create(): T {
+            return this.context.getBean(dependency);
+        }
+    };
+}
