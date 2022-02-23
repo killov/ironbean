5e71bfa369e3a26279a6d30b6837bbe1b6d9068e
Zdeněk Mazurák <z.mazurak@seznam.cz>
2022-02-23 23:34:44 +0100

    react withAutowired

--- packages/ironbean-react/src/index.ts
+++ packages/ironbean-react/src/index.ts
@@ -1,5 +1,17 @@
-import {createContext, createElement, FunctionComponent, ReactNode, useContext, useEffect, useState} from "react";
+import React, {
+    createContext,
+    createElement,
+    forwardRef,
+    FunctionComponent,
+    ReactNode,
+    useContext,
+    useEffect,
+    useState
+} from "react";
 import {ApplicationContext, Dependency, getBaseApplicationContext} from "ironbean";
+import {IPlugin, registerPlugin} from "ironbean/dist/api";
+import {ComponentContainer} from "ironbean/dist/componentContainer";
+import {Container} from "ironbean/dist/container";
 
 export function useBean<T>(dependency: Dependency<T>): T {
     const componentAppContext = useContext(reactContext)
@@ -23,3 +35,39 @@ interface IContextProviderProps {
 export const ContextProvider: FunctionComponent<IContextProviderProps> = (props) => {
     return createElement(reactContext.Provider, {value: props.context}, props.children);
 }
+
+const tajny = Symbol();
+const tajny2 = Symbol();
+const tajny3 = Symbol();
+
+class Plugin implements IPlugin {
+    getComponentContainerForClassInstance(Class: object): ComponentContainer | undefined {
+        if (!Class[tajny2]) {
+            return undefined;
+        }
+        if (Class[tajny2] !== Class.props[tajny]) {
+            Class[tajny2] = Class.props[tajny];
+            Class[tajny3] = new ComponentContainer(Class.props[tajny].getBean(Container));
+        }
+
+        return Class[tajny3];
+    }
+}
+
+registerPlugin(new Plugin());
+
+export function withAutowired(): <T extends React.ComponentClass<any>>(component: T) => T {
+    return <T extends React.ComponentClass<P>, P>(component: T) => {
+        class b extends component {}
+
+        b[tajny2] = true;
+
+        return forwardRef((props, ref) => {
+            const context = useBean(ApplicationContext);
+            const p = {...props};
+            p[tajny] = context;
+            return React.createElement(b as any, p);
+        }) as any;
+    }
+}
+
