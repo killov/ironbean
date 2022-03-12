f402f9485235b79071c39e0a3df538d045b2a03c
Zdeněk Mazurák <z.mazurak@seznam.cz>
2022-03-12 11:28:58 +0100

    example app

--- examples/app/src/App.tsx
+++ examples/app/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {useBean} from "ironbean-react";
-import {autowired, component, type} from "ironbean";
+import {autowired, component} from "ironbean";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
 @component
 class A {
@@ -12,33 +13,61 @@ class A {
 @component
 class B {
     @autowired  a!: A;
+
+    text: string = "";
     c = 10;
 }
 
 const App = () => {
     const b = useBean(B);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            {b.a.g}
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div className="exampleContainer">
+      <BrowserRouter>
+          <Link to={"first"}>firts</Link>
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<HomePage />}/>
+            </Routes>
 
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </BrowserRouter>
   );
 }
+
+const HomePage: React.FC<{}> = () => {
+    const b = useBean(B);
+    return (
+        <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                {b.a.g}
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <div className="exampleContainer">
+
+            </div>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </>
+    );
+}
+
+const FirstPage: React.FC = () => {
+    const b = useBean(B);
+    return (
+        <>
+            first page
+            <input value={b.text} onChange={(e) => {
+                b.text = e.target.value;
+            }}/>
+        </>
+    );
+}
+
 
 export default App;
