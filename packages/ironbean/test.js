4a168654b7bc7a4a4d2ac88f7b927e8bc7b20733
Zdeněk Mazurák <z.mazurak@seznam.cz>
2022-09-08 23:20:23 +0200

    refoldering

--- packages/ironbean/test.js
+++
@@ -1,3 +0,0 @@
-var internalsTesting_1 = require("./internalsTesting");
-Object.defineProperty(exports, "getBaseTestingContext", { enumerable: true, get: function () { return internalsTesting_1.getBaseTestingContext; } });
-Object.defineProperty(exports, "TestingContext", { enumerable: true, get: function () { return internalsTesting_1.TestingContext; } });
